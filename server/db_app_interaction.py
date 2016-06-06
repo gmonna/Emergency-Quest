264c70f723ad5b31caa411ad8a6a40da93d00b6d
gmonna <hilok80@yahoo.it>
2016-06-06 12:10:12 +0200

    Add files via upload

--- server/db_app_interaction.py
+++ server/db_app_interaction.py
@@ -11,15 +11,15 @@ def get_code(bcod):
   """
   Check if bcod inserted by user is present
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimizedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT bcod FROM CODE WHERE bcod=?"
   cursor.execute(sql, (bcod, ))
   bcod = cursor.fetchone()
-  
+
   conn.close()
   return bcod
 
@@ -30,11 +30,11 @@ def code_byemail(mail):
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimizedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT bcod FROM USERS WHERE mail=?"
   cursor.execute(sql, (mail, ))
   bcod = cursor.fetchone()
-  
+
   conn.close()
   return bcod
 
@@ -42,51 +42,51 @@ def sign_in(mail, password):
   """
   Log in to the system
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT nome, cognome FROM USERS WHERE mail=? AND password=?"
-  
+
   cursor.execute(sql, (mail, password))
   user = cursor.fetchone()
-    
+
   conn.close()
   return user
-  
+
 def sign_up(bcod, mail, password, name, surname):
   """
   Sign up to the system
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
   sql = "INSERT INTO USERS(bcod, mail, password, name, surname) VALUES (?, ?, ?, ?, ?)"
-  
+
   try:
     cursor.execute(sql, (bcod, mail, password, name, surname))
     conn.commit()
   except Exception, e:
     print str(e)
     conn.rollback()
-    
+
   conn.close()
-  
+
 def lost_password(mail):
   """
   Send password to user
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT password FROM USERS WHERE mail=?"
-  
+
   cursor.execute(sql, (mail, ))
   password = cursor.fetchone()
-  
+
   sender = 'info@emergencyquest.com'
   receivers = [mail]
 
@@ -100,27 +100,27 @@ def lost_password(mail):
 
   try:
     smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receivers, message)         
+    smtpObj.sendmail(sender, receivers, message)
     print "Successfully sent email"
-  except SMTPException:
+  except SMTPException, e:
     print "Error: unable to send email"
-   
+
   conn.close()
-  
-def get_settings(mail)
+
+def get_settings(mail):
   """
   Get current settings for the user
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT perimeter, colour, song, doct, message, auto_clean FROM PREFERENCES WHERE mail=?"
 
   cursor.execute(sql, (mail, ))
   settings = cursor.fetchone()
-  
+
   conn.close()
   return settings
 
@@ -128,34 +128,34 @@ def set_settings(mail, perimeter, colour, song, doct, message, auto_clean, first
   """
   Set preferences distinguishing between new user and already registered user
   """
-  
+
   if (first=="y"):
     sql = "INSERT INTO PREFERENCES(perimeter, colour, song, doct, message, auto_clean, mail) VALUES (?, ?, ?, ?, ?, ?, ?)"
   else:
     sql = "UPDATE PREFERENCES SET perimeter=?, colour=?, song=?, doct=?, message=?, auto_clean=? WHERE mail=?"
-    
+
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
-  
+
   try:
     cursor.execute(sql, (perimeter, colour, song, doct, message, auto_clean, mail))
     conn.commit()
   except Exception, e:
     print str(e)
     conn.rollback()
-    
+
   conn.close()
-  
+
 def get_history(mail):
   """
   Get patient's history of notifications
   """
   history = []
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT read, data, ora, message FROM HISTORY WHERE mail=?"
 
   cursor.execute(sql, (mail, ))
@@ -168,18 +168,18 @@ def history_all_read(mail):
   Set all notifications as read after opening history page
   """
   history = []
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "UPDATE HISTORY SET read=y WHERE mail=?"
 
   cursor.execute(sql, (mail, ))
   history = cursor.fetchall()
 
   return history
-  
+
 def delete_history_doneappo(mail):
   """
   Delete patient's history and done appointments from database every *x* time
@@ -187,10 +187,10 @@ def delete_history_doneappo(mail):
 
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
-  
+
   sql = "DELETE FROM HISTORY WHERE mail=?"
   sql2 = "DELETE FROM CALENDAR WHERE mail=? AND done=y"
-  
+
   try:
     cursor.execute(sql, (mail, ))
     cursor.execute(sql2, (mail, ))
@@ -198,7 +198,7 @@ def delete_history_doneappo(mail):
   except Exception, e:
     err = str(e)
     conn.rollback()
-    
+
   conn.close()
 
 def get_calendar(mail):
@@ -206,11 +206,11 @@ def get_calendar(mail):
   Get patient's appointments
   """
   calendar = []
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT code, title, done, data, ora FROM CALENDAR WHERE mail=?"
 
   cursor.execute(sql, (mail, ))
@@ -218,17 +218,17 @@ def get_calendar(mail):
 
   conn.close()
   return calendar
-  
+
 def select_appointment(mail, code):
   """
   Get patient's specific appointment
   """
   calendar = []
-  
+
   conn = sqlite3.connect("db/database.db")
   conn.text_factory = sqlite3.OptimezedUnicode
   cursor = conn.cursor()
-  
+
   sql = "SELECT title, description, data, ora, message, priority, repeat FROM CALENDAR WHERE mail=? AND code=?"
 
   cursor.execute(sql, (mail, code ))
@@ -241,53 +241,53 @@ def set_appointment(mail, description, title, data, ora, message, priority, repe
   """
   Add an appointment to the calendar
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
   done = "n"
   sql = "INSERT INTO CALENDAR(mail, description, title, done, data, ora, message, priority, repeat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-  
+
   try:
-    cursor.execute(sql, (mail, decription, title, done, data, ora, message, priority, repeat))
+    cursor.execute(sql, (mail, description, title, done, data, ora, message, priority, repeat))
     conn.commit()
   except Exception, e:
     print str(e)
     conn.rollback()
-    
+
   conn.close()
 
 def delete_appointment(mail, code):
   """
   Add an appointment to the calendar
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
   sql = "DELETE FROM CALENDAR WHERE mail=? AND code=?"
-  
+
   try:
     cursor.execute(sql, (mail, code))
     conn.commit()
   except Exception, e:
     print str(e)
     conn.rollback()
-    
+
   conn.close()
-  
+
 def update_appo(mail, code, title, description, data, ora, message, priority, repeat):
   """
   Update an appointment into the calendar
   """
-  
+
   conn = sqlite3.connect("db/database.db")
   cursor = conn.cursor()
   sql = "UPDATE CALENDAR SET title=? AND description=? AND data=? AND ora=? AND message=? AND priority=? AND repeat=? WHERE mail=? AND code=?"
-  
+
   try:
     cursor.execute(sql, (title, description, data, ora, message, priority, repeat, mail, code))
     conn.commit()
   except Exception, e:
     print str(e)
     conn.rollback()
-    
+
   conn.close()
--- server/mobile_rest.py
+++ server/mobile_rest.py
@@ -5,39 +5,44 @@ Created on May 30, 2016
 @author: gmonna
 """
 
-from flask import Flask, jsonify, abort, request, Response
+from flask import Flask, jsonify, abort, session, request, Response
 import db_app_interaction
 
 app = Flask(__name__)
+
+app.secret_key = 'F12Zr47jyXRX@H!jmM]Lwf?KT'
 
 #----------- REST APIs FOR APP ----------#
 @app.route('/rest_api/v1.0/signup', methods=['POST'])
 def new_user():
   insert_request = request.json
-  
+
   if (insert_request is not None) and ('name' and 'surname' and 'mail' and 'password' and 'bcod') in insert_request:
     bcod = insert_request['bcod']
     bc = db_app_interaction.get_code(bcod)
     if bc is None:
       abort(404)
-      
+
     mail = insert_request['mail']
+    em = db_app_interaction.code_byemail(mail)
+    if em is not None:
+      abort(302) #--already existing user--#
     password = insert_request['password']
     name = insert_request['name']
     surname = insert_request['surname']
-    
+
     db_app_interaction.sign_up(bcod, mail, password, name, surname)
     return Response(status=200)
-  
+
   abort(403)
-  
+
 @app.route('/rest_api/v1.0/signin', methods=['GET'])
 def log_in():
   mail = request.args.get('mail')
   bcod = db_app_interaction.code_bymail(mail)
-  if bcode is None:
+  if bcod is None:
     abort(404)
-    
+
   password = request.args.get('password')
   user = db_app_interaction.sign_in(mail, password)
   session['mail'] = mail
@@ -49,38 +54,38 @@ def log_in():
 def al_log_in():
   if(session['mail']):
     return Response(status=200)
-  
+
 @app.route('/rest_api/v1.0/logout', methods=['GET'])
 def log_out():
   del session['mail']
   del session['name']
   del session['surname']
   return Response(status=200)
-  
+
 @app.route('/rest_api/v1.0/lost_password', methods=['GET'])
 def retrieve_pass():
   mail = request.args.get('mail')
   bcod = db_app_interaction.code_bymail(mail)
-  if bcode is None:
+  if bcod is None:
     abort(404)
-    
+
   db_app_interaction.lost_password(mail)
   return Response(status=200)
-  
+
 @app.route('/rest_api/v1.0/get_settings', methods=['GET'])
 def load_settings():
   mail = session['mail']
-  
+
   set_list = db_app_interaction.get_settings(mail)
-  
+
   return jsonify({'settings':prepare_for_json(set_list)})
-  
+
 @app.route('/rest_api/v1.0/set_settings', methods=['POST'])
 def settings():
   setting_req = request.json
-  
+
   if (setting_req is not None) and ('perimeter' and 'colour' and 'song' and 'doct' and 'message' and 'auto_clean' and 'first') in setting_req:
-    
+
     mail = session['mail']
     perimeter = setting_req['perimeter']
     colour = setting_req['colour']
@@ -89,43 +94,43 @@ def settings():
     message = setting_req['message']
     auto_clean = setting_req['auto_clean']
     first = setting_req['first']
-    
+
     db_app_interaction.set_settings(mail, perimeter, colour, song, doct, message, auto_clean, first)
     return Response(status=200)
-  
+
   abort(403)
 
 @app.route('/rest_api/v1.0/get_history', methods=['GET'])
 def load_history():
   mail = session['mail']
-  
+
   history = []
   notif = db_app_interaction.get_history(mail)
   for item in notif:
     his = prepare_for_json(item)
     history.append(his)
-  
+
   db_app_interaction.history_all_read(mail)
   return jsonify({'history':history})
-  
+
 @app.route('/rest_api/v1.0/get_calendar', methods=['GET'])
 def load_calendar():
   mail = session['mail']
-  
+
   calendar = []
   cal = db_app_interaction.get_calendar(mail)
   for item in cal:
     cl = prepare_for_json(item)
     calendar.append(cl)
-  
+
   return jsonify({'calendar':calendar})
 
 @app.route('/rest_api/v1.0/calendar/insert', methods=['POST'])
 def insert_appointment():
   req = request.json
-  
+
   if (req is not None) and ('title' and 'description' and 'data' and 'ora' and 'message' and 'priority' and 'repeat') in req:
-    
+
     mail = session['mail']
     titolo = req['title']
     data = req['data']
@@ -134,32 +139,32 @@ def insert_appointment():
     description = req['description']
     priority = req['priority']
     repeat = req['repeat']
-    
+
     db_app_interaction.set_appointment(mail, description, titolo, data, ora, message, priority, repeat)
     return Response(status=200)
-  
+
   abort(403)
 
 @app.route('/rest_api/v1.0/calendar/<int:code>', methods=['GET'])
-def view_appointment(int(code)):
+def view_appointment(code):
   mail = session['mail']
-  
+
   appo = db_app_interaction.select_appointment(mail, int(code))
   appointment = prepare_for_json(appo)
   return jsonify({'appointment':appointment})
-  
+
 @app.route('/rest_api/v1.0/calendar/<int:code>', methods=['DELETE'])
-def delete_appointment(int(code)):
+def delete_appointment(code):
   mail = session['mail']
-  
+
   db_app_interaction.delete_appointment(mail, int(code))
   return Response(status=200)
-  
+
 @app.route('/rest_api/v1.0/calendar/<int:code>', methods=['PUT'])
-def update_appointment(int(code)):
+def update_appointment(code):
   mail = session['mail']
   update_req = request.json
-  
+
   if update_req is not None and ('code' and 'title' and 'description' and 'data' and 'ora' and 'message' and 'priority' and 'repeat') in update_req:
     code = update_req['code']
     titolo = update_req['title']
@@ -169,12 +174,12 @@ def update_appointment(int(code)):
     description = update_req['description']
     priority = update_req['priority']
     repeat = update_req['repeat']
-    
+
     db_app_interaction.update_appo(mail, int(code), titolo, description, data, ora, message, priority, repeat)
     return Response(status=200)
-  
+
   abort(403)
-  
+
 @app.route('/rest_api/v1.0/get_position', methods=['GET']) #--TODO--#
 def get_position():
   return None
@@ -182,25 +187,25 @@ def get_position():
 def prepare_for_json(item):
   tot = dict()
 
-  if (len(item)==4):
+  if len(item)==4:
     tot['read'] = item[0]
     tot['data'] = item[1]
     tot['ora'] = item[2]
     tot['message'] = item[3]
-  if (len(item)==5):
+  if len(item)==5:
     tot['done'] = item[0]
     tot['titolo'] = item[1]
     tot['data'] = item[2]
     tot['ora'] = item[3]
     tot['code'] = item[4]
-  if (len(item)==6):
+  if len(item)==6:
     tot['perimeter'] = item[0]
     tot['colour'] = item[1]
     tot['song'] = item[2]
     tot['doct'] = item[3]
     tot['message'] = item[4]
     tot['auto_clean'] = item[5]
-  if (len(item)==7):
+  if len(item)==7:
     tot['title'] = item[0]
     tot['description'] = item[1]
     tot['data'] = item[2]
@@ -208,6 +213,9 @@ def prepare_for_json(item):
     tot['message'] = item[4]
     tot['priority'] = item[5]
     tot['repeat'] = item[6]
-  
+
   return tot
 #------ APIs END ------#
+
+if __name__ == '__main__':
+  app.run(debug=True)
